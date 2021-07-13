0bf7f566e752dec23b8f7f20f1423ce762210087
Jozef Lelic <jozef@salsitasoft.com>
2021-07-14 01:48:32 +0200

    Remove unused import

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import {useState} from 'react'
-import logo from './logo.svg';
 import './App.css';
 import LoadLogForm from './components/LoadLogForm'
 import PlayerTable from './components/PlayerTable'
